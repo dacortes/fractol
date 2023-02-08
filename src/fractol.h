489003d6c9cc63a9f9973b7bcc62e925e18bb277
Dannyel Cortes Romero <dacortes@car11s3.42barcelona.com>
2023-02-08 14:06:11 +0100

    actualizacion .h

--- src/fractol.h
+++ src/fractol.h
@@ -6,29 +6,33 @@
 /*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 17:34:26 by dacortes          #+#    #+#             */
-/*   Updated: 2023/02/01 12:18:08 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/02/08 14:05:50 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
-//-------------------------- Librerias ---------------------------------------//
-//# include"./miniLibX/mlx.h"
+// ================================= LIBRARIES ============================== //
+
 #include"../miniLibX/mlx.h"
 # include"../libft/libft.h"
 # include"../libft/ft_printf.h"
 # include<math.h>
-//-------------------------- Macros ------------------------------------------//
+
+// ================================= MACROS ================================= //
+
 # define WIDTH 1000
 # define HEIGHT 1000 
 # define ZOOM (float)1.1
-//-------------------------- Colors_printf -----------------------------------//
+
+// ================================= COLORS ================================= //
+
 # define E "\033[m"        //end
 # define R "\033[1;31m"    //red
 # define G "\033[1;32m"    //green
 # define Y "\033[1;33m"    //yellow
 # define B "\033[1;34m"    //blue
-//-------------------------- Structures --------------------------------------//
+// ================================= STRUCTURES ============================= //
 //Windows && Image
 typedef struct s_win {
 	void	*mlx;
@@ -62,7 +66,7 @@ typedef struct s_color
 	int		b;
 	int		t;
 }	t_color;
-//-------------------------- Funciones ---------------------------------------//
+// ================================= FUNCTIONS ============================== //
 //Check_error
 void	error_agv(void);
 int		check_arg(char *tmp);
