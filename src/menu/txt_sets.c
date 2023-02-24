81fb1d457b2c4298e39a141d1a640ba9f6a2543e
Dannyel Cortes Romero <dacortes@car15s5.42barcelona.com>
2023-02-24 19:44:10 +0100

    txt de maldelbrot finalizado

--- src/menu/txt_sets.c
+++ src/menu/txt_sets.c
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 18:20:36 by dacortes          #+#    #+#             */
-/*   Updated: 2023/02/24 19:36:51 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/02/24 19:43:36 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,10 @@ void	txt(int x, int y, t_fractol *f)
 		put_str(f, "es el conjunto de valores en el plano", x - 10, y + 7);
 		put_str(f, "complejo cuyo valor absoluto", x - 10, y + 9);
 		put_str(f, "permanece por debajo de algun valor", x - 10, y + 11);
-		put_str(f, "finito en las iteraciones de un😅", x - 10, y + 13);
-		/* 			 
-		
-		 miembro de la familia general de los polinomios mónicos de una variable recursivos", x, y);*/
-		
+		put_str(f, "finito en las iteraciones de un", x - 10, y + 13);
+		put_str(f, "miembro de la familia general de los", x - 10, y + 15);
+		put_str(f, "polinomios monicos de una variable", x - 10, y + 17);
+		put_str(f, "recursivos.", x - 10, y + 19);
 	}
 	else if (f->var.set == JULIA)
 		put_str(f, "JULIA", x, y);
