00e546874035020549599432914c25e5636a4584
Dannyel Cortes Romero <dacortes@car15s5.42barcelona.com>
2023-02-24 19:56:47 +0100

    txt julia finalizado

--- src/menu/txt_sets.c
+++ src/menu/txt_sets.c
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 18:20:36 by dacortes          #+#    #+#             */
-/*   Updated: 2023/02/24 19:43:36 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/02/24 19:52:35 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,16 @@ void	txt(int x, int y, t_fractol *f)
 		put_str(f, "recursivos.", x - 10, y + 19);
 	}
 	else if (f->var.set == JULIA)
+	{
 		put_str(f, "JULIA", x, y);
+		put_str(f, "Los conjuntos de Julia, asi llamados", x - 10, y + 5);
+		put_str(f, "por el matematico Gaston Julia son", x - 10, y + 7);
+		put_str(f, "una familia de conjuntos fractales", x - 10, y + 9);
+		put_str(f, "que se obtienen al estudiar el", x - 10, y + 11);
+		put_str(f, "comportamiento de los numeros", x - 10, y + 13);
+		put_str(f, "complejos al ser iterados por una", x - 10, y + 15);
+		put_str(f, "funcion holomorfa.", x - 10, y + 17);
+	}
 	else if (f->var.set == BURNING_SHIP)
 		put_str(f, "BURNING SHIP", x, y);
 }
